6bc091f51c67823373e55526ac43ae6f2d210fc6
robarca10 <rohanpg2004@gmail.com>
2024-02-01 11:25:55 +0530

    Refactor ClientHandler class to include message broadcasting and client removal

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -29,7 +29,57 @@ public class ClientHandler implements Runnable{
 
     @Override
     public void run(){
+        String messageFromClient;
 
+        while(socket.isConnected()){
+            try{
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            }
+            catch(IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
     }
+
+    public void broadcastMessage(String messageToSend){
+        for( ClientHandler clientHandler : clientHandlers){
+            try{
+                 if(!clientHandler.clientUsername.equals(clientUsername)){
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                 }
+            }
+            catch(IOException e){
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler(){
+        clientHandlers.remove(this);
+        broadcastMessage("Server", clientUsername + " has left the chat");
+    }
+
+publilc void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+    removeClientHandler();
+    try{
+        if(bufferedReader != null){
+            bufferedReader.close();
+        }
+
+        if(bufferedWriter!=null){
+            bufferedWriter.close();
+        }
+        if(socket != null){
+            socket.close();
+        }
+    }
+    catch( IOException e){
+        e.printStackTrace();
+    }
+}
 
 }
